7ca96abe9575a065f45129490b9abca0f1082e15
Fyodor Sheremetyev <sheremetyev@gmail.com>
2012-04-22 23:45:53 +0400

    Use getopt implementation.

--- peg.gyp
+++ peg.gyp
@@ -14,6 +14,9 @@
           'include_dirs': [
             'win',
           ],
+          'sources': [
+            'win/getopt.c',
+          ],
         }],
       ],
     },
--- win/unistd.h
+++ win/unistd.h
@@ -1,8 +1,6 @@
 #ifndef _UNISTD_H
 #define _UNISTD_H
 
-extern int optind,opterr,optopt;
-extern char *optarg;
-int getopt(int argc, char *const argv[], const char *options);
+#include "getopt.h"
 
 #endif // _UNISTD_H
